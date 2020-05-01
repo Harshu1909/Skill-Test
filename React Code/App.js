be5a2c1d1cb67393cfd1b744a30e547e290c85d1
Harsh Patel <Hpatel0330@conestogac.on.ca>
2020-05-01 12:35:16 -0400

    All codes

---
+++ React Code/App.js
@@ -0,0 +1,68 @@
+/*Example of SQLite Database in React Native*/
+import React from 'react';
+
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+
+import HomeScreen from './Pages/HomeScreen';
+import RegisterUser from './Pages/RegisterUser';
+import UpdateUser from './Pages/UpdateUser';
+import ViewUser from './Pages/ViewUser';
+import ViewAllUser from './Pages/ViewAllUser';
+import DeleteUser from './Pages/DeleteUser';
+import Constants from 'expo-constants';
+
+Constants.manifest.extra.fact === 'kittens are cool';
+
+const App = createStackNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'HomeScreen',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  View: {
+    screen: ViewUser,
+    navigationOptions: {
+      title: 'View User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  ViewAll: {
+    screen: ViewAllUser,
+    navigationOptions: {
+      title: 'View All User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Update: {
+    screen: UpdateUser,
+    navigationOptions: {
+      title: 'Update User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Register: {
+    screen: RegisterUser,
+    navigationOptions: {
+      title: 'Register User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Delete: {
+    screen: DeleteUser,
+    navigationOptions: {
+      title: 'Delete User',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff',
+    },
+  },
+});
+export default createAppContainer(App);
